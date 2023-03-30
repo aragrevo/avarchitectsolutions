a61ac10d7e18a95eb217f69b4fd6dadce579cc9f
Eduardo Vergara <eduver_san@hotmail.com>
2023-03-30 18:56:12 -0500

    feat: :construction: Add TopBAr

---
+++ src/db/data.js
@@ -0,0 +1,14 @@
+export const socialNetworks = [
+  {
+    name: 'Instagram',
+    href: 'https://www.instagram.com/av.architectsolutions/',
+  },
+  {
+    name: 'Facebook',
+    href: 'https://www.instagram.com/av.architectsolutions/',
+  },
+  {
+    name: 'Email',
+    href: 'mailto:hugo_andres_v@hotmail.com',
+  },
+];
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        OO1ED3: '#001ED3',
+        '41444B': '#41444B',
+      },
+    },
+    fontFamily: {
+      Lato: ['Lato', 'sans-serif', 'system-ui'],
+      Kanit: ['Kanit', 'sans-serif', 'system-ui'],
+    },
+  },
+  plugins: [],
+};
